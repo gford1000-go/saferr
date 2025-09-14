f0ee50f8798e046f16117b25744df932c3110603
Geoff Ford <geoff.golang.12@gmail.com>
2025-09-14 10:30:30 +0100

    Updated respPool[U] to remove access to sync.Pool, to avoid errors where resp[U] are not reset before being added back to the pool

--- resp.go
+++ resp.go
@@ -5,47 +5,49 @@ import (
 )
 
 type resp[U any] struct {
-	id   uint64
-	data *U
-	err  error
-	pool *respPool[U]
+	id       uint64
+	data     *U
+	err      error
+	returner func(x *resp[U])
 }
 
 // close will trigger this instance to be reset and added back to the pool it came from (if known)
 func (r *resp[U]) close() {
-	if r.pool != nil {
-		r.pool.Put(r)
+	if r.returner != nil {
+		r.returner(r)
 	}
 }
 
 // Since we expect a lot of traffic between Requestors and Responders,
 // use pools to minimise object creation
 type respPool[U any] struct {
-	pool sync.Pool
-}
-
-// Get is a slight variance to idiomatic Go for sync.Pool,
-// returning fully initialised resp[U] instance
-func (p *respPool[U]) Get(id uint64, u *U, err error) *resp[U] {
-	r := p.pool.Get().(*resp[U])
-
-	r.data, r.err, r.pool, r.id = u, err, p, id
-	return r
-}
-
-// Put ensures that the returned instance is reset before reuse
-func (p *respPool[U]) Put(x *resp[U]) {
-	x.data, x.err, x.pool, x.id = nil, nil, nil, 0
-	p.pool.Put(x)
+	Get func(id uint64, u *U, err error) *resp[U]
+	Put func(x *resp[U])
 }
 
 func newRespPool[U any]() *respPool[U] {
-	return &respPool[U]{
-		pool: sync.Pool{
-			New: func() any {
-				var v any = new(resp[U])
-				return v
-			},
+
+	p := sync.Pool{
+		New: func() any {
+			var v any = new(resp[U])
+			return v
 		},
+	}
+
+	putter := func(x *resp[U]) {
+		x.data, x.err, x.returner, x.id = nil, nil, nil, 0
+		p.Put(x)
+	}
+
+	getter := func(id uint64, u *U, err error) *resp[U] {
+		r := p.Get().(*resp[U])
+
+		r.data, r.err, r.returner, r.id = u, err, putter, id
+		return r
+	}
+
+	return &respPool[U]{
+		Get: getter,
+		Put: putter,
 	}
 }
